b1f65c1943722f422b36f3198edeb188671c02d3
Zaripov Radmir <eosqqone@gmail.com>
2022-02-13 10:58:11 +0500

    Открытие модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,5 +1,29 @@
+import { animate } from "./helpers"
+
 const modal = () => {
 
+const callbackBtn = document.querySelectorAll('.callback-btn')
+console.log(callbackBtn);
+const modalCallback = document.querySelector('.modal-callback')
+const modalOverlay = document.querySelector('.modal-overlay ')
+
+callbackBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault()
+        animate({
+            duration: 300,
+            timing (timeFraction) {
+                return timeFraction
+            },
+            draw (progress) {
+                modalCallback.style.opacity = progress
+                modalOverlay.style.opacity = progress
+            }
+        })
+        modalCallback.style.display = 'block'
+        modalOverlay.style.display = 'block'
+    })
+})
 }
 
 export default modal
