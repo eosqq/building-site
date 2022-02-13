315e370ea0db665a2292d639ecb7f0058d551202
Zaripov Radmir <eosqqone@gmail.com>
2022-02-13 16:07:13 +0500

    Модальное окно для button-services

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -7,8 +7,9 @@ const modalCallback = document.querySelector('.modal-callback')
 const modalOverlay = document.querySelector('.modal-overlay ')
 const closeModal = document.querySelector('.modal-close')
 const carouselElems = document.querySelectorAll('.fancyboxModal ')
+const buttonServices = document.querySelectorAll('.button-services')
 
-const animation = () => {
+const openAnimation = () => {
     animate({
         duration: 300,
         timing (timeFraction) {
@@ -19,23 +20,28 @@ const animation = () => {
             modalOverlay.style.opacity = progress
         }
     })
+    modalCallback.style.display = 'block'
+    modalOverlay.style.display = 'block'
 }
 
 callbackBtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         e.preventDefault()
-        animation()
-        modalCallback.style.display = 'block'
-        modalOverlay.style.display = 'block'
+        openAnimation()
     })
 })
 
 carouselElems.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         e.preventDefault()
-        animation()
-        modalCallback.style.display = 'block'
-        modalOverlay.style.display = 'block'
+        openAnimation()
+    })
+})
+
+buttonServices.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault()
+        openAnimation()
     })
 })
 
