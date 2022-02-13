c9f1ff4c75471bf4b4f7a9ca73c4672a07c6e687
Zaripov Radmir <eosqqone@gmail.com>
2022-02-13 12:38:51 +0500

    Реализация слайдера без дотсов

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,5 +1,39 @@
 const slider = () => {
-
-}
-
-export default slider
+    const slider = document.querySelector('.top-slider')
+    const slides = document.querySelectorAll('.item')
+    const contents = slider.querySelectorAll('.table')
+    
+    const timeInterval = 3000;
+    let currentSlide = 0;
+    let interval;
+    
+    const prevSlide = (elems, index) => {
+        elems[index].style.display = 'none'
+    }
+    
+    const nextSlide = (elems, index) => {
+        elems[index].style.display = 'block'
+    }
+    
+    const autoSlide  = () => {
+        prevSlide(slides, currentSlide);
+        currentSlide++;
+        if (currentSlide >= slides.length) {
+          currentSlide = 0;
+        }
+        nextSlide(slides, currentSlide);
+    }
+    
+    const startSlide = (timer) => {
+        interval = setInterval(autoSlide , timer);
+    }
+    
+    contents.forEach((content) => {
+        content.style.opacity = 1;
+        content.style.visibility = 'visible'
+    })
+    
+    startSlide(timeInterval)
+    }
+    
+    export default slider
