6ef6dde5bdd22c4a266a5db532bb1c033285bedd
Zaripov Radmir <eosqqone@gmail.com>
2022-02-13 18:29:02 +0500

    Блок ЧАВО в виде аккордеона

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import slider from './modules/slider'
 import modal from './modules/modal'
 import carousel from './modules/carousel'
+import accordion from './modules/accordion'
 
 slider ()
 modal ()
 carousel ()
+accordion ()
---
+++ src/modules/accordion.js
@@ -0,0 +1,36 @@
+const accordion = () => {
+
+const accordion = document.querySelector('.accordeon')
+const elements = accordion.querySelectorAll('.element')
+const elementsContent = accordion.querySelectorAll('.element-content')
+const accordeonTitles = accordion.querySelectorAll('.title')
+
+    const removeClassActive = () => {
+        elements.forEach((element) => {
+            element.classList.remove('active')
+        })
+    }
+
+    const showContentOfActiveElement = () => {
+        elementsContent.forEach((content) => {
+            content.style.display = 'none'
+            if (content.closest('.active')){
+                content.style.display = 'block'
+            }
+          })
+    }
+    
+    elements.forEach((element) => { 
+        element.addEventListener(('click'), () => {  
+        if (element.classList.contains('active')) {
+            element.classList.remove('active')
+        } else {
+          removeClassActive()
+          element.classList.add('active')
+        }
+      showContentOfActiveElement()        
+      })
+    })
+}
+
+export default accordion
