009311cf9d16ddbab4aa89b76de19723e25878fe
Zaripov Radmir <eosqqone@gmail.com>
2022-02-13 12:27:23 +0500

    Удалил все, чтобы делать через ветки

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,39 +1,5 @@
-import { animate } from "./helpers"
-
 const modal = () => {
-
-const callbackBtn = document.querySelectorAll('.callback-btn')
-const modalCallback = document.querySelector('.modal-callback')
-const modalOverlay = document.querySelector('.modal-overlay ')
-const closeModal = document.querySelector('.modal-close')
-
-callbackBtn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        e.preventDefault()
-        animate({
-            duration: 300,
-            timing (timeFraction) {
-                return timeFraction
-            },
-            draw (progress) {
-                modalCallback.style.opacity = progress
-                modalOverlay.style.opacity = progress
-            }
-        })
-        modalCallback.style.display = 'block'
-        modalOverlay.style.display = 'block'
-    })
-})
-
-closeModal.addEventListener('click', (e) => {
-    modalCallback.style.display = 'none'
-    modalOverlay.style.display = 'none'
-})
-
-modalOverlay.addEventListener('click', (e) => {
-    modalCallback.style.display = 'none'
-    modalOverlay.style.display = 'none'
-})
+    
 }
 
 export default modal
