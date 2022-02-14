a052b10571bfd70a44b4be59cc565d5dcb8d9c76
Zaripov Radmir <eosqqone@gmail.com>
2022-02-14 14:46:38 +0500

    Реализована отправка формы

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import modal from './modules/modal'
 import carousel from './modules/carousel'
 import accordion from './modules/accordion'
 import arrowUp from './modules/arrowUp'
-import validation from './modules/validation'
 import sendForm from './modules/sendForm'
+import validation from './modules/validation'
 
 slider ()
 modal ()
@@ -12,32 +12,4 @@ carousel ()
 accordion ()
 arrowUp ()
 validation ()
-sendForm({
-    formId: 'form1',
-    someElem: [
-        {
-            type: 'block',
-            id: 'total'
-        }
-    ]
-},
-
-)
-sendForm({
-    formId: 'form2',
-    someElem: [
-        {
-            type: 'block',
-            id: 'total'
-        }
-    ]
-})
-sendForm({
-    formId: 'form3',
-    someElem: [
-        {
-            type: 'block',
-            id: 'total'
-        }
-    ]
-})
+sendForm()
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,5 +1,91 @@
-const sendForm = ({ formId, someElem = [] }) => {
+const sendForm = () => {
+    const modal = document.querySelector(".modal-callback");
+    const modalOverlay = document.querySelector('.modal-overlay');
+    const form = document.querySelector('[name="form-callback"]');
+    const formElements = form.querySelectorAll("input");
+    const statusBlock = document.createElement("div");
+    const loadText = "Идёт отправка...";
+    const successText = "Спасибо! Наш менеджер с вами свяжется";
+    const errorText = "Ошибка";
     
-}
+    const validate = (list) => {
+        let success = true;
+        list.forEach((input) => {
+            
+            if (input.name == "tel") {  
+                if (input.value.length <= 5 && input.value.length <= 11) {
+                    success = false
+                }
+            }
+  
+            if (input.name == "fio") {
+                if (input.value.length <= 2) {
+                    success = false
+                }
+            }
+        })
+        return success
+    }
+   
+    const sendData = (data) => {
+        return fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((res) => res.json)
+    }
 
-export default sendForm
+    const cleanData = () => {
+        formElements.forEach((input) => {
+            if (input.name == "tel" || input.name == "fio") {
+                input.value = ""
+            }        
+        })
+    }
+
+    const submitForm = () => {
+      const formData = new FormData(form)
+      const formBody = {}
+
+      statusBlock.textContent = loadText
+      form.append(statusBlock)
+
+      formData.forEach((value, key) => {
+        formBody[key] = value
+      })
+
+      if (validate(formElements)) {
+        sendData(formBody)
+          .then(() => {
+            statusBlock.textContent = successText
+            setTimeout(() => {
+                modal.style.display = "none"
+                modalOverlay.style.display = "none"
+                statusBlock.textContent = ""
+            }, 2000)
+            cleanData()
+          })
+          .catch(() => {
+            statusBlock.textContent = errorText
+          });
+      } else {
+        alert("Данные не валидны!")
+        statusBlock.textContent = ""
+      }
+    }
+
+    try {
+      if (!form) {
+        throw new Error("Верните форму на место!")
+      }
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        submitForm()
+      })
+    } catch (error) {
+    console.log(error.message);
+    }
+}
+export default sendForm;
